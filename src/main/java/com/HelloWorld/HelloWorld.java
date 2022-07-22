4457416ac5e233e69ef7a0e246566c28c7a33a9b
Baiyang Li <baiyanl@amazon.com>
2022-07-22 12:30:01 -0700

    test corretto11 x86

--- src/main/java/com/HelloWorld/HelloWorld.java
+++ src/main/java/com/HelloWorld/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
     @RequestMapping("/")
     public String index(){
         String s = System.getProperty("java.version");
-        s = "new verision";
+        s = "corretto11 x86";
         return s;
     }
 }
